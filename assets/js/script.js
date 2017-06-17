e2ca15504f304c6ff3d65308e72e5ee01f9566e8
Dylan Thomson <djthomson88@gmail.com>
2017-06-17 17:41:41 -0400

    Start code for New Quote Button
    -Create generateQuote() function and call on "click"
    -Update page with new quote information
    -In the future, generateQuote() will get a quote from an API

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$("#newQuote").on("click", function() {
+	//Generate New Quote
+	var newQuote = generateQuote();
+
+	//Update span with new quote
+	$("#quote").text(newQuote.quote);
+
+	//Update footer with person name
+	$("#source").text(newQuote.source);
+
+	//Change background color
+});
+
+function generateQuote() {
+	return {
+		quote: "Fancy new quote that's totall random!",
+		source: "Random Fancy Guy"
+	};
+}
