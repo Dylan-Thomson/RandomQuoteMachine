277ca2455b036258da1a4910eb88535112b80a5b
Dylan Thomson <djthomson88@gmail.com>
2017-06-17 21:51:37 -0400

    New Quote Changes Colors
    -Link to jQuery Color CDN
    -Choose a random color from a hardcoded list
    -Animate color change for buttons, text, and background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 $("#newQuote").on("click", function() {
 	generateQuote();
-
-	//Change background color
+	changeColor();
 });
 
 $("#twitter").on("click", function() {
@@ -9,12 +8,6 @@ $("#twitter").on("click", function() {
 	var author = $("#author").text();
 	window.location.href = "http://twitter.com/home?status=" + quote + " - " + author;
 });
-
-// $("#facebook").on("click", function() {
-// 	var quote = $("#quote").text();
-// 	var author = $("#author").text();
-// 	window.location.href = "http://www.facebook.com/share.php?u=" + window.location.pathname;
-// });
 
 function generateQuote() {
 	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
@@ -27,4 +20,37 @@ function generateQuote() {
     	$("#author").text("Unknown");
     }
   });
+}
+
+var colors = [
+	"rgb(0, 102, 153)",
+	"rgb(51, 51, 255)",
+	"rgb(153, 51, 51)",
+	"rgb(204, 153, 0)",
+	"rgb(0, 153, 51)",
+	"rgb(0, 153, 153)",
+	"rgb(0, 102, 204)",
+	"rgb(204, 102, 0)",
+	"rgb(204, 0, 204)"
+];
+
+function changeColor() {
+	var color;
+	var currentColor = $("body").css("backgroundColor");
+	color = colors[getRandomIntInclusive(0, colors.length-1)];
+	console.log(color, currentColor);
+
+	while(color === currentColor) {
+		color = colors[getRandomIntInclusive(0, colors.length-1)];
+	}
+
+	$("body").animate({backgroundColor: color}, 1500);
+	$(".btn-lg").animate({backgroundColor: color}, 1500);
+	$(".col-xs-12").animate({color: color}, 1500);
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
