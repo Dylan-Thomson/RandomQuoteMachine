58e1f2434ed3bd42c22f5e97a52f5a34c01fdd50
Dylan Thomson <djthomson88@gmail.com>
2017-06-17 20:06:35 -0400

    getJSON
    -New Quote button gets a random quote from the Forismatic API, then
    updates HTML with new quote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,12 @@
 $("#newQuote").on("click", function() {
-	//Generate New Quote
-	var newQuote = generateQuote();
-
-	//Update span with new quote
-	$("#quote").text(newQuote.quote);
-
-	//Update footer with person name
-	$("#source").text(newQuote.source);
-
+	generateQuote();
 	//Change background color
 });
 
 function generateQuote() {
-	return {
-		quote: "Fancy new quote that's totally random!",
-		source: "Random Fancy Guy"
-	};
+	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+	 $.getJSON(url, function(data) {
+    $("#quote").text(data.quoteText);
+    $("#author").text(data.quoteAuthor);
+  });
 }
