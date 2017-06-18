436c7115322fcb389c8dee868de2e41d1ca3d479
Dylan Thomson <djthomson88@gmail.com>
2017-06-17 20:33:23 -0400

    Tweet this quote
    -Twitter button passes quote and author to Twitter, allowing user to
    easily tweet the quote.
    -Facebook does not seem to accept URLs in the same way, so it is
    disabled for now.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 $("#newQuote").on("click", function() {
 	generateQuote();
+
 	//Change background color
 });
+
+$("#twitter").on("click", function() {
+	var quote = $("#quote").text();
+	var author = $("#author").text();
+	window.location.href = "http://twitter.com/home?status=" + quote + " - " + author;
+});
+
+// $("#facebook").on("click", function() {
+// 	var quote = $("#quote").text();
+// 	var author = $("#author").text();
+// 	window.location.href = "http://www.facebook.com/share.php?u=" + window.location.pathname;
+// });
 
 function generateQuote() {
 	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
