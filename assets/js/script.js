d9c68228718dc404d3167111ae82d06b5a9d5cc1
Dylan Thomson <djthomson88@gmail.com>
2017-06-17 20:35:44 -0400

    Handle unknown author
    -If quoteAuthor string is empty, write "Unknown" to the page instead of
    nothing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,11 @@ function generateQuote() {
 	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 	 $.getJSON(url, function(data) {
     $("#quote").text(data.quoteText);
-    $("#author").text(data.quoteAuthor);
+    if(data.quoteAuthor.length > 0) {
+    	$("#author").text(data.quoteAuthor);
+    }
+    else {
+    	$("#author").text("Unknown");
+    }
   });
 }
